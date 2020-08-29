45fb99b27bf68572815290b6f94ba0f27e15ad58
agarwal-paras <parasa263@gmail.com>
2020-08-29 16:01:27 +0530

    added new model for ticket

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from enum import Enum
+
+class Ticket(models.Model):
+    
+    ticket_id = models.AutoField(primary_key = True)
+    ticket_time = models.DateTimeField('date published')
+    user_name = models.CharField(max_length = 20)
+    user_contact = models.CharField(max_length = 10)
+    
+    class Ticket_validate(Enum):
+    	Valid = 'VALID'
+    	Invalid = 'INVALID'
+
+    ticket_status = models.CharField(max_length = 7, choices = {('Valid', 'VALID'), ('Invalid', 'INVALID')}, default = 'Valid')
